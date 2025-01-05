2affdb84bea0eeb6b95442b59bee620cb0a57309
花朝九日's desktop <982086195@qq.com>
2025-01-05 15:49:54 +0800

    增加PWA

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import {VitePWA} from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,7 +15,32 @@ export default defineConfig({
     }),
     Components({
       resolvers: [ElementPlusResolver()]
-    })
+    }),
+    VitePWA({
+      registerType: 'autoUpdate', // 自动更新 Service Worker
+      injectRegister: 'auto',
+      manifest: {
+        name: '花朝九日',
+        short_name: '花朝',
+        description: '花朝九日的博客',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone', // 使应用独立运行，无浏览器 UI
+        start_url: '/',
+        icons: [
+          {
+            src: '/avatar.jpg',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/avatar.jpg',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
   ],
   server: {
     port: 8181,
