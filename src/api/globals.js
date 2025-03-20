c742de61b046d2953eaa2339c88d7e365036cb5f
花朝九日's laptop <982086195@qq.com>
2025-03-20 14:25:24 +0800

    接入QQ登录

--- src/api/globals.js
+++ src/api/globals.js
@@ -1,7 +1,7 @@
 import {ref} from 'vue';
 // 定义全局变量
 export const HomePage = ref(1); // 当前页
-export const HomeArticles = ref(5); // 每页文章数
+export const HomeArticles = ref(10); // 每页文章数
 export const WEB_URL = "https://flowerinfire.com/#"
 export const formatDate = (dateString) => {
     const date = new Date(dateString);
